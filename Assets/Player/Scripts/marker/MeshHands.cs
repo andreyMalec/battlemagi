89a4d8925d630305041761ffa547df7f8d578fbf
andreyMalec <andrey193728@yandex.ru>
2026-06-29 18:48:41 +0300

    arrow charge

--- Assets/Player/Scripts/marker/MeshHands.cs
+++ Assets/Player/Scripts/marker/MeshHands.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 public class MeshHands : MonoBehaviour {
     private static readonly int Scale = Shader.PropertyToID("_Scale");
     [SerializeField] private Transform invocation;
+    [SerializeField] private Renderer mesh;
 
-    public void Bind() {
-        GetComponentInParent<Player>().meshController.invocation = invocation;
-        foreach (var material in GetComponentInChildren<SkinnedMeshRenderer>().materials) {
+    private void Awake() {
+        foreach (var material in mesh.materials) {
             if (material.HasFloat(Scale)) {
                 material.SetFloat(Scale, 0.35f);
             }
         }
+    }
+
+    public void Bind() {
+        GetComponentInParent<Player>().meshController.invocation = invocation;
     }
 }
